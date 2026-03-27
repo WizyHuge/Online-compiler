d88e75626ac6b82ad02d2d2e37d59106e2796afa
WizyHuge <hom.semen@yandex.ru>
2026-03-27 18:31:48 +0500

    register and login

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 # ______________import_____________
-from flask import Flask, request, jsonify, render_template, Response, url_for
+from flask import Flask, request, jsonify, render_template, Response, url_for, redirect
+from flask_login import login_user, login_required, logout_user, LoginManager
+
+from forms.login_form import LoginForm
+from forms.register_form import RegisterForm
+
+from data.users import User
+from data import db
 
 import redis
 import json
@@ -10,13 +17,27 @@ import time
 #_______________init_______________
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = 'f959fc589c1b0c1e5fcb03c8d480d14499902624862252d3b30de1dd40f9bc45'
 r = redis.Redis(host="localhost", port=6379, decode_responses=True)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
 
 # ______________routes____________
 
+@login_manager.user_loader
+def load_user(user_id):
+    db_sess = db.create_session()
+    return db_sess.get(User, user_id)
+
 @app.route('/')
 def index():
-    return render_template('index.html', title='Main page')
+    return render_template('index.html', title='Main Page')
+
+@app.route('/editor')
+@login_required
+def editor():
+    return render_template('editor.html', title='Editor')
 
 @app.route("/api/run", methods=["POST"])
 def run_code():
@@ -53,10 +74,48 @@ def stream(task_id):
 
     return Response(generate(), mimetype="text/event-stream")
 
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_session = db.create_session()
+        user = db_session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template("login.html",
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template("login.html", title="Авторизация", form=form)
 
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_sess = db.create_session()
+        if db_sess.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            about=form.about.data
+        )
+        user.set_password(form.password.data)
+        db_sess.add(user)
+        db_sess.commit()
+        return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
+    
 
 # ______________start_____________
 def main():
+    db.global_init("db/database.db")
     app.run(debug=True)
 
 if __name__ == "__main__":
